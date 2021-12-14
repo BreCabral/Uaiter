ce529a398ee8f33c0ecd3ffca924cf5854d45c77
Breno <breno.ara.cabral@gmail.com>
2021-12-14 12:06:33 -0300

    Aplicativo antes do teste no servidor, necessario configurar servidor com os topicos do aplicativo.

--- app/src/main/java/com/example/uaiter/MainActivity.java
+++ app/src/main/java/com/example/uaiter/MainActivity.java
@@ -2,138 +2,129 @@ package com.example.uaiter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
+    static String MQTTHOST = "mqtt://192.168.0.6:1883";
+    static String USERNAME = "uaiter";
+    static String PASSWORD = "uaiter";
+    MqttAndroidClient client;
+    TextView txtStatus;
+    TextView txtFluxo;
+    TextView txtVolume;
+    String topicoValvula = "valvula";
+    String topicoFluxo = "vazao";
+    String topicoVolume = "volume";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    //aaaaaaaaaaaaaa
-    int frente = 0, esquerda = 0, direita = 0, parado = 0, garraAberta = 0, modoAutomatico = 0;
-    boolean valvula = false;
-    public void mudarEstadoValvula(View v) {
-        if(valvula)
-            valvula = false;
-        else
-            valvula = true;
-        atualizar(v);
-    }
+        txtStatus = findViewById(R.id.tvStatusMqtt);
+        txtFluxo = findViewById(R.id.tvMedicaoFluxo);
+        txtVolume = findViewById(R.id.tvMedicaoVolume);
 
-    public void atualizar(View view) {
-        // Verifica dados recebidos
-        SincronizaDados sincroniza = new SincronizaDados();
-        sincroniza.execute(new String[]{""});
-        // Envia comandos
-        String comandosDirecao = "?direcao";
-        if (valvula)
-            comandosDirecao += "=a";
-        else
-            comandosDirecao += "=f";
-        EnviaComandos controleDirecao = new EnviaComandos();
-        controleDirecao.execute(new String[]{comandosDirecao});
-    }
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), MQTTHOST, clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
 
-    String ip = "";
-    public void conectarESP(View v){
-        EditText IP = (EditText) findViewById(R.id.etIP);
-        ip = IP.getText().toString();
-        atualizar(v);
-    }
-    private class EnviaComandos extends AsyncTask<String, Void, String> {
-        // dispara a requisição
-        @Override
-        protected String doInBackground(String... params) {
-            HttpURLConnection con = null;
-            try {
-                con = (HttpURLConnection) ( new URL("http://" + ip + "/controle" + params[0])).openConnection();
-                con.setRequestMethod("GET");
-                con.setDoInput(true);
-                con.setDoOutput(true);
-                con.connect();
-                StringBuffer buffer = new StringBuffer();
-                InputStream is = con.getInputStream();
-                is.close();
-                con.disconnect();
-                return "Conectado";
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "Desconectado";
-            }
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    txtStatus.setText("conectado");
+                    setSubscrition();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    txtStatus.setText("desconectado");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
-        // trata os dados retornados
-        @Override
-        protected void onPostExecute(String retorno) {
-            super.onPostExecute(retorno);
-            EditText etEstadoComunicacao = (EditText) findViewById(R.id.etComunicacao);
-            etEstadoComunicacao.setText(retorno);
-        }
-    }
-    private class SincronizaDados extends AsyncTask<String, Void, String> {
-        // dispara a requisição
-        @Override
-        protected String doInBackground(String... params) {
-            HttpURLConnection con = null;
-            try {
-                con = (HttpURLConnection) ( new URL("http://" + ip + "/sincronizacao")).openConnection();
-                con.setRequestMethod("GET");
-                con.setDoInput(true);
-                con.setDoOutput(true);
-                con.connect();
-                StringBuffer buffer = new StringBuffer();
-                InputStream is = con.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                String line = null;
-                while ( (line = br.readLine()) != null )
-                    buffer.append(line + "\r\n");
-                is.close();
-                con.disconnect();
-                return buffer.toString();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "Erro na comunicação";
+
+
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                txtStatus.setText("desconectado");
             }
-        }
-        // trata os dados retornados
-        @Override
-        protected void onPostExecute(String retorno) {
-            EditText etEstadoComunicacao = (EditText) findViewById(R.id.etComunicacao);
-            if (retorno.equals("Desconectado"))
-            {
-                etEstadoComunicacao.setText(retorno);
-                return;
-            }
-            etEstadoComunicacao.setText("Conectado");
-            String[] sensoresSeparados = retorno.split("\n");
-            for (int i = 0; i < sensoresSeparados.length; i++)
-            {
-                String[] sensorargumento = sensoresSeparados[i].split("=");
-                switch (sensorargumento[0])
-                {
-                    case "sensorLinha":
-                        EditText etEstadoLinha = (EditText) findViewById(R.id.etEstadoLinha);
-                        etEstadoLinha.setText("Caminho: " + sensorargumento[1]);
-                        break;
-                    case "sensorDistancia":
-                        EditText etEstadoDistancia = (EditText) findViewById(R.id.etDistancia);
-                        etEstadoDistancia.setText("Distancia: " + sensorargumento[1]);
-                        break;
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                if(topic.toString().equals(topicoFluxo)){
+                    String texto = new String(message.getPayload());
+                    txtFluxo.setText(texto);
+                }
+                if(topic.toString().equals(topicoVolume)){
+                    String texto = new String(message.getPayload());
+                    txtVolume.setText(texto);
                 }
             }
-            super.onPostExecute(retorno);
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                txtStatus.setText("Dados Recebidos");
+            }
+        });
+
+    }
+
+    private void setSubscrition(){
+        try{
+            client.subscribe(topicoFluxo,0);
+        }catch (MqttException e) {
+            e.printStackTrace();
+            txtStatus.setText("Erro Sub Fluxo");
+        }
+        try{
+            client.subscribe(topicoVolume,0);
+        }catch (MqttException e) {
+            e.printStackTrace();
+            txtStatus.setText("Erro Sub Volume");
         }
     }
-    //aaaaaaaaaaaaaa
+
+    boolean estadoValvula = false;
+    public void controleValvula(View v){
+        String topic = topicoValvula;
+        if (estadoValvula){
+            String message = "fechar";
+            try {
+                client.publish(topic, message.getBytes(),0,false    );
+                txtStatus.setText("Comando Fechar Enviado");
+            } catch (MqttException e) {
+                e.printStackTrace();
+                txtStatus.setText("desconectado");
+            }
+        }else{
+            String message = "abrir";
+            try {
+                client.publish(topic, message.getBytes(),0,false    );
+                txtStatus.setText("Comando Abrir Enviado");
+            } catch (MqttException e) {
+                e.printStackTrace();
+                txtStatus.setText("desconectado");
+            }
+        }
+    }
 }
